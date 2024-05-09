4dffd19d9efebf26e37ae765750b607dd5d2bc4d
Muhammad Usman <m.usman095@gmail.com>
2024-05-09 17:26:11 +0500

    commited

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -95,7 +95,7 @@ export default function HomePage() {
               <li onClick={() => handleScrollTo("courses")}>Courses</li>
               <li onClick={() => handleScrollTo("aboutUs")}>About Us</li>
               <li onClick={() => handleScrollTo("features")}>Features</li>
-              <li onClick={() => handleScrollTo("calendar")}>Calendar</li>
+              {/* <li onClick={() => handleScrollTo("calendar")}>Calendar</li> */}
               <li onClick={() => handleScrollTo("bmi")}>BMI</li>
             </ul>
           </div>
