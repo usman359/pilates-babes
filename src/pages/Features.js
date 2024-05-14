cd68ae4bfa908680cbd37d33cb04c3eadb8202b9
Muhammad Usman <m.usman095@gmail.com>
2024-05-14 11:44:48 +0500

    commited

--- src/pages/Features.js
+++ src/pages/Features.js
@@ -12,7 +12,10 @@ function Features() {
           <h4>Progression</h4>
           <p>
             Progression is the journey towards improvement and growth, marked by
-            continuous advancement and development.
+            continuous advancement and development. It's the steady climb up the
+            ladder of success, fueled by determination and perseverance. With
+            each step forward, new horizons emerge, revealing opportunities for
+            further expansion and evolution.
           </p>
         </div>
       </div>
@@ -25,7 +28,11 @@ function Features() {
           <h4>Workout</h4>
           <p>
             Workout involves physical activity designed to improve strength,
-            endurance, flexibility, or overall fitness levels.
+            endurance, flexibility, or overall fitness levels. It's a dynamic
+            process that challenges the body and mind, pushing individuals to
+            surpass their previous limits and reach new heights of performance.
+            Whether it's lifting weights, or any other form of exercise, the
+            goal remains the same.
           </p>
         </div>
       </div>
@@ -38,7 +45,11 @@ function Features() {
           <h4>Nutrition</h4>
           <p>
             Nutrition is the process of providing or obtaining the necessary
-            food and nutrients for health, growth, and vitality.
+            food and nutrients for health, growth, and vitality. It's about
+            nourishing the body with the essential building blocks it needs to
+            function optimally. This includes not only macronutrients like
+            carbohydrates, proteins, and fats but also micronutrients such as
+            vitamins and minerals.
           </p>
         </div>
       </div>
