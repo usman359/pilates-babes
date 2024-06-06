6c39fdf0cc67c21525bd4fbed48f415344fca319
Muhammad Usman <m.usman095@gmail.com>
2024-06-06 11:58:12 +0500

    Commited

--- src/pages/Classes.js
+++ src/pages/Classes.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import ClassesCart from "../components/ClassesCart";
+import Footer from "./Footer";
 
 function Classes() {
   const [classItems, setClassItems] = useState([]);
@@ -52,135 +53,138 @@ function Classes() {
   }, [classItems]);
 
   return (
-    <main className="bg-[#e7e5e4] p-16">
-      <div className="relative">
-        {/* Pilates babes heading */}
-        <h1 className="mb-16 text-center text-5xl uppercase">Classes</h1>
-        {/* Shopping bag */}
-        <div className="cursor-pointer" onClick={handleShowCart}>
-          <img
-            src="shopping-bag.png"
-            alt="Shopping cart"
-            className="absolute right-0 top-0 h-10 w-10"
-          />
+    <>
+      <main className="bg-[#e7e5e4] p-16">
+        <div className="relative">
+          {/* Pilates babes heading */}
+          <h1 className="mb-16 text-center text-5xl uppercase">Classes</h1>
+          {/* Shopping bag */}
+          <div className="cursor-pointer" onClick={handleShowCart}>
+            <img
+              src="shopping-bag.png"
+              alt="Shopping cart"
+              className="absolute right-0 top-0 h-10 w-10"
+            />
+            <div>
+              {classItems.length > 0 && (
+                <span className="absolute -right-1 -top-1 flex h-6 w-6 items-center justify-center rounded-full bg-black p-2 text-sm text-white">
+                  {classItems.length}
+                </span>
+              )}
+            </div>
+          </div>
+
           <div>
-            {classItems.length > 0 && (
-              <span className="absolute -right-1 -top-1 flex h-6 w-6 items-center justify-center rounded-full bg-black p-2 text-sm text-white">
-                {classItems.length}
-              </span>
+            {/* Cart Box */}
+            {showCart && (
+              <div
+                ref={ref}
+                className="absolute right-0 top-16 cursor-default border bg-white p-8 text-xl"
+              >
+                {/* Header */}
+                <header className="mb-8 flex items-center justify-between gap-12">
+                  <div className="flex items-center gap-2">
+                    <img src="check.png" alt="Check" className="h-8 w-8" />
+                    <p>Item has been added to your shopping basket</p>
+                  </div>
+                  <div>
+                    <img
+                      src="close.png"
+                      alt="Close"
+                      className="h-4 w-4 cursor-pointer"
+                      onClick={() => setShowCart(false)}
+                    />
+                  </div>
+                </header>
+
+                {/* Main */}
+                <main>
+                  {classItems.map((item) => (
+                    <div
+                      className="mb-8 flex items-center justify-between"
+                      key={item.id}
+                    >
+                      <div className="flex items-center justify-center gap-4">
+                        <img
+                          src={item.imageSrc}
+                          alt="Sticker"
+                          className="h-14 w-12"
+                        />
+                        <h6>{item.title}</h6>
+                        <span>({item.quantity})</span>
+                      </div>
+                      <div>
+                        <img
+                          src="bin.png"
+                          alt="bin"
+                          className="h-6 w-6 cursor-pointer"
+                          onClick={() => handleItemDelete(item.id)}
+                        />
+                      </div>
+                    </div>
+                  ))}
+
+                  <button className="mb-4 block w-full rounded-full bg-black py-4 text-white">
+                    Check out
+                  </button>
+                </main>
+              </div>
             )}
           </div>
-        </div>
 
-        <div>
-          {/* Cart Box */}
-          {showCart && (
-            <div
-              ref={ref}
-              className="absolute right-0 top-16 cursor-default border bg-white p-8 text-xl"
-            >
-              {/* Header */}
-              <header className="mb-8 flex items-center justify-between gap-12">
-                <div className="flex items-center gap-2">
-                  <img src="check.png" alt="Check" className="h-8 w-8" />
-                  <p>Item has been added to your shopping basket</p>
-                </div>
-                <div>
-                  <img
-                    src="close.png"
-                    alt="Close"
-                    className="h-4 w-4 cursor-pointer"
-                    onClick={() => setShowCart(false)}
-                  />
-                </div>
-              </header>
-
-              {/* Main */}
-              <main>
-                {classItems.map((item) => (
-                  <div
-                    className="mb-8 flex items-center justify-between"
-                    key={item.id}
-                  >
-                    <div className="flex items-center justify-center gap-4">
-                      <img
-                        src={item.imageSrc}
-                        alt="Sticker"
-                        className="h-14 w-12"
-                      />
-                      <h6>{item.title}</h6>
-                      <span>({item.quantity})</span>
-                    </div>
-                    <div>
-                      <img
-                        src="bin.png"
-                        alt="bin"
-                        className="h-6 w-6 cursor-pointer"
-                        onClick={() => handleItemDelete(item.id)}
-                      />
-                    </div>
-                  </div>
-                ))}
-
-                <button className="mb-4 block w-full rounded-full bg-black py-4 text-white">
-                  Check out
-                </button>
-              </main>
-            </div>
-          )}
-        </div>
-
-        <main className="flex flex-col gap-8">
-          <ClassesCart
-            id={1}
-            type="singleClass"
-            imageSrc="courses/singleClass.jpeg"
-            title="Classic Pilates - Single Class 55 min online"
-            price="22.00"
-            soldOut={false}
-            description="Get to know the classic Pilates method on the mat or deepen your practice.
+          <main className="flex flex-col gap-8">
+            <ClassesCart
+              id={1}
+              type="singleClass"
+              imageSrc="courses/singleClass.jpeg"
+              title="Classic Pilates - Single Class 55 min online"
+              price="22.00"
+              soldOut={false}
+              description="Get to know the classic Pilates method on the mat or deepen your practice.
           As always, you don't have to be a professional. I look forward to seeing you.
           You can, but don't have to, turn on your camera. ;)"
-            classItems={classItems}
-            setclassItems={setClassItems}
-          />
-          <ClassesCart
-            id={2}
-            type="privateClass"
-            imageSrc="courses/privateClass.jpeg"
-            title="Private Class - 55 min online"
-            price="100.00"
-            soldOut={false}
-            description="Individual session tailored to your needs. To arrange an appointment,
+              classItems={classItems}
+              setclassItems={setClassItems}
+            />
+            <ClassesCart
+              id={2}
+              type="privateClass"
+              imageSrc="courses/privateClass.jpeg"
+              title="Private Class - 55 min online"
+              price="100.00"
+              soldOut={false}
+              description="Individual session tailored to your needs. To arrange an appointment,
           please contact me by email at hi@pilatesbabes.com."
-            classItems={classItems}
-            setclassItems={setClassItems}
-          />
-          <ClassesCart
-            id={3}
-            type="slot1Hamburg"
-            imageSrc="courses/slot1Hamburg.jpeg"
-            title="Pilates Babes Brunch Slot 1 Hamburg"
-            price="65.00"
-            soldOut={true}
-            description="Finally the time has come again and the sale for the next Pilates Brunch 2024 is entering the next round."
-            classItems={classItems}
-            setclassItems={setClassItems}
-          />
-          <ClassesCart
-            id={4}
-            type="slot2Hamburg"
-            imageSrc="courses/slot2Hamburg.jpeg"
-            title="Pilates Babes Brunch Slot 2 Hamburg"
-            price="65.00"
-            soldOut={true}
-            description="Finally the time has come again and the sale for the next Pilates Brunch 2024 is entering the next round."
-            classItems={classItems}
-            setclassItems={setClassItems}
-          />
-        </main>
-      </div>
-    </main>
+              classItems={classItems}
+              setclassItems={setClassItems}
+            />
+            <ClassesCart
+              id={3}
+              type="slot1Hamburg"
+              imageSrc="courses/slot1Hamburg.jpeg"
+              title="Pilates Babes Brunch Slot 1 Hamburg"
+              price="65.00"
+              soldOut={true}
+              description="Finally the time has come again and the sale for the next Pilates Brunch 2024 is entering the next round."
+              classItems={classItems}
+              setclassItems={setClassItems}
+            />
+            <ClassesCart
+              id={4}
+              type="slot2Hamburg"
+              imageSrc="courses/slot2Hamburg.jpeg"
+              title="Pilates Babes Brunch Slot 2 Hamburg"
+              price="65.00"
+              soldOut={true}
+              description="Finally the time has come again and the sale for the next Pilates Brunch 2024 is entering the next round."
+              classItems={classItems}
+              setclassItems={setClassItems}
+            />
+          </main>
+        </div>
+      </main>
+      <Footer />
+    </>
   );
 }
 
--- src/pages/OfferedProducts.js
+++ src/pages/OfferedProducts.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState, useEffect } from "react";
 import Products from "../components/Products";
+import Footer from "./Footer";
 
 function OfferedProducts() {
   const [productItems, setProductItems] = useState([]);
@@ -48,125 +49,128 @@ function OfferedProducts() {
   }, [productItems]);
 
   return (
-    <main className="bg-[#e7e5e4] p-16">
-      <div className="relative">
-        {/* Pilates babes heading */}
-        <h1 className="mb-16 text-center text-5xl uppercase">Our Products</h1>
-        <div className="cursor-pointer" onClick={handleShowCart}>
-          <img
-            src="shopping-bag.png"
-            alt="Shopping cart"
-            className="absolute right-0 top-0 h-10 w-10"
-          />
+    <>
+      <main className="bg-[#e7e5e4] p-16">
+        <div className="relative">
+          {/* Pilates babes heading */}
+          <h1 className="mb-16 text-center text-5xl uppercase">Our Products</h1>
+          <div className="cursor-pointer" onClick={handleShowCart}>
+            <img
+              src="shopping-bag.png"
+              alt="Shopping cart"
+              className="absolute right-0 top-0 h-10 w-10"
+            />
+            <div>
+              {productItems.length > 0 && (
+                <span className="absolute -right-1 -top-1 flex h-6 w-6 items-center justify-center rounded-full bg-black p-2 text-sm text-white">
+                  {productItems.length}
+                </span>
+              )}
+            </div>
+          </div>
+
           <div>
-            {productItems.length > 0 && (
-              <span className="absolute -right-1 -top-1 flex h-6 w-6 items-center justify-center rounded-full bg-black p-2 text-sm text-white">
-                {productItems.length}
-              </span>
+            {/* Cart Box */}
+            {showCart && (
+              <div
+                ref={ref}
+                className="absolute right-0 top-16 cursor-default border bg-white p-8 text-xl"
+              >
+                {/* Header */}
+                <header className="mb-8 flex items-center justify-between gap-12">
+                  <div className="flex items-center gap-2">
+                    <img src="check.png" alt="Check" className="h-8 w-8" />
+                    <p>Item has been added to your shopping basket</p>
+                  </div>
+                  <div>
+                    <img
+                      src="close.png"
+                      alt="Close"
+                      className="h-4 w-4 cursor-pointer"
+                      onClick={() => setShowCart(false)}
+                    />
+                  </div>
+                </header>
+
+                {/* Main */}
+                <main>
+                  {productItems.map((item) => (
+                    <div
+                      className="mb-8 flex items-center justify-between"
+                      key={item.id}
+                    >
+                      <div className="flex items-center justify-center gap-4">
+                        <img
+                          src={item.imageSrc}
+                          alt="Sticker"
+                          className="h-14 w-12"
+                        />
+                        <h6>{item.title}</h6>
+                        <span>({item.quantity})</span>
+                      </div>
+                      <div>
+                        <img
+                          src="bin.png"
+                          alt="bin"
+                          className="h-6 w-6 cursor-pointer"
+                          onClick={() => handleItemDelete(item.id)}
+                        />
+                      </div>
+                    </div>
+                  ))}
+
+                  <button className="mb-4 block w-full rounded-full bg-black py-4 text-white">
+                    Check out
+                  </button>
+                </main>
+              </div>
             )}
           </div>
-        </div>
 
-        <div>
-          {/* Cart Box */}
-          {showCart && (
-            <div
-              ref={ref}
-              className="absolute right-0 top-16 cursor-default border bg-white p-8 text-xl"
-            >
-              {/* Header */}
-              <header className="mb-8 flex items-center justify-between gap-12">
-                <div className="flex items-center gap-2">
-                  <img src="check.png" alt="Check" className="h-8 w-8" />
-                  <p>Item has been added to your shopping basket</p>
-                </div>
-                <div>
-                  <img
-                    src="close.png"
-                    alt="Close"
-                    className="h-4 w-4 cursor-pointer"
-                    onClick={() => setShowCart(false)}
-                  />
-                </div>
-              </header>
-
-              {/* Main */}
-              <main>
-                {productItems.map((item) => (
-                  <div
-                    className="mb-8 flex items-center justify-between"
-                    key={item.id}
-                  >
-                    <div className="flex items-center justify-center gap-4">
-                      <img
-                        src={item.imageSrc}
-                        alt="Sticker"
-                        className="h-14 w-12"
-                      />
-                      <h6>{item.title}</h6>
-                      <span>({item.quantity})</span>
-                    </div>
-                    <div>
-                      <img
-                        src="bin.png"
-                        alt="bin"
-                        className="h-6 w-6 cursor-pointer"
-                        onClick={() => handleItemDelete(item.id)}
-                      />
-                    </div>
-                  </div>
-                ))}
-
-                <button className="mb-4 block w-full rounded-full bg-black py-4 text-white">
-                  Check out
-                </button>
-              </main>
-            </div>
-          )}
-        </div>
-
-        <main className="flex flex-col gap-8">
-          <Products
-            id={1}
-            type="sweatshirt"
-            imageSrc="sweat-shirt.jpeg"
-            title="THE ORIGINAL Pilates Babes Sweatshirt"
-            price="89.00"
-            soldOut={true}
-            description={`THE ORIGINAL Pilates Babes Unisex Sweatshirt:
+          <main className="flex flex-col gap-8">
+            <Products
+              id={1}
+              type="sweatshirt"
+              imageSrc="sweat-shirt.jpeg"
+              title="THE ORIGINAL Pilates Babes Sweatshirt"
+              price="89.00"
+              soldOut={true}
+              description={`THE ORIGINAL Pilates Babes Unisex Sweatshirt:
           screen printing 
           Set-in sleeve
           2x2 rib knit at neckline, sleeve ends and bottom hem
           Herringbone neckband
           `}
-            productItems={productItems}
-            setProductItems={setProductItems}
-          />
-          <Products
-            id={2}
-            type="sticker"
-            imageSrc="sticker.jpeg"
-            title="Pilates Babes Logo Sticker"
-            price="1.50"
-            soldOut={false}
-            description="Your highly coveted Pilates Babes sticker is now finally available in the shop!"
-            productItems={productItems}
-            setProductItems={setProductItems}
-          />
-          <Products
-            id={3}
-            type="cap"
-            imageSrc="cap.jpeg"
-            title="Pilates Babes Baseball Cap"
-            price="39.00"
-            soldOut={false}
-            description="The baseball cap is the perfect accessory for sports or everyday life."
-            productItems={productItems}
-            setProductItems={setProductItems}
-          />
-        </main>
-      </div>
-    </main>
+              productItems={productItems}
+              setProductItems={setProductItems}
+            />
+            <Products
+              id={2}
+              type="sticker"
+              imageSrc="sticker.jpeg"
+              title="Pilates Babes Logo Sticker"
+              price="1.50"
+              soldOut={false}
+              description="Your highly coveted Pilates Babes sticker is now finally available in the shop!"
+              productItems={productItems}
+              setProductItems={setProductItems}
+            />
+            <Products
+              id={3}
+              type="cap"
+              imageSrc="cap.jpeg"
+              title="Pilates Babes Baseball Cap"
+              price="39.00"
+              soldOut={false}
+              description="The baseball cap is the perfect accessory for sports or everyday life."
+              productItems={productItems}
+              setProductItems={setProductItems}
+            />
+          </main>
+        </div>
+      </main>
+      <Footer />
+    </>
   );
 }
 
