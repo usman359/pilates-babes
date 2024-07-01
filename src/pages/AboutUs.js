5b2886d15fc22f2dee3b0fc8a6a065732f444c0d
usman359 <m.usman095@gmail.com>
2024-07-01 11:08:11 +0500

    Commited

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -4,25 +4,28 @@ import styles from "./AboutUs.module.css";
 function AboutUs() {
   return (
     <div className={styles.aboutUsContainer} id="aboutUs">
-      <div className="absolute top-[20%] flex gap-12">
-        <div>
-          <img src="hero-img.jpg" alt="Hello" className={styles.image} />
+      <div className="absolute top-[20%] flex flex-col gap-12">
+        <div className="flex justify-center">
+          <img
+            src="hero-img.jpg"
+            alt="Body mirror by sema"
+            className={styles.image}
+          />
         </div>
         <div className={styles.textBox}>
           <h1>10 Years Experience</h1>
           <p>Labore vero lorem eos sed aliquy ipsum aliquy sed.</p>
 
           <div className={styles.accomplishment}>
-            <div>
+            <div className="text-center">
               <div className={styles.icon}>
                 <ion-icon name="barbell-outline"></ion-icon>
               </div>
               <h3>Certified GYM Center</h3>
               <p>Ipsum sanctu dolor ipsum dolore sit et kasd duo</p>
-              <button>Learn More</button>
             </div>
 
-            <div>
+            <div className="text-center">
               <div className={styles.icon}>
                 <ion-icon name="trophy-outline"></ion-icon>
               </div>
@@ -31,6 +34,7 @@ function AboutUs() {
             </div>
           </div>
         </div>
+        <button className="flex justify-center">Learn More</button>
       </div>
     </div>
   );
